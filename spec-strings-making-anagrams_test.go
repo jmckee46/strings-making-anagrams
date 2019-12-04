bd17ae6e88a0de32c7fae7e2ec1cc7eb556309ba
John McKee <jmckee3@mac.com>
2019-12-04 13:51:38 -0800

    working version with logging

--- spec-strings-making-anagrams_test.go
+++ spec-strings-making-anagrams_test.go
@@ -1,7 +1,19 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestStringsMakingAnagrams(t *testing.T) {
+	a := "cde"
+	b := "abc"
 
+	deletions := makeAnagram(a, b)
+
+	fmt.Println(deletions)
+
+	if deletions != 4 {
+		t.Errorf("got %d instead of 4", deletions)
+	}
 }
--- strings-making-anagrams.go
+++ strings-making-anagrams.go
@@ -1,3 +1,37 @@
 package main
 
+import "fmt"
+
+func makeAnagram(a string, b string) int32 {
+	var deletions int32
+
+	stringCountA := stringCount(a)
+	stringCountB := stringCount(b)
+
+	for i := 0; i < 26; i++ {
+		deletions += absoluteValue(stringCountA[i] - stringCountB[i])
+		fmt.Println(deletions)
+	}
+
+	return deletions
+}
+
+func stringCount(x string) []int32 {
+	stringCount := make([]int32, 26)
+	for _, value := range x {
+		stringCount[int(value)-97]++
+	}
+
+	fmt.Println(stringCount)
+	return stringCount
+}
+
+func absoluteValue(x int32) int32 {
+	if x < 0 {
+		return -x
+	}
+
+	return x
+}
+
 func main() {}
